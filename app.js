d119cc8abb242ae81021c0ecf77be802b4961cdd
danedehaan <danedehaan@qubitsec.com>
2020-07-17 17:23:11 +0900

    쿠팡 오픈 api

---
+++ app.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const { generateHmac } = require('./hmacGenerator');
+
+const REQUEST_METHOD = "POST";
+const DOMAIN = "https://api-gateway.coupang.com";
+const URL = "/v2/providers/affiliate_open_api/apis/openapi/v1/deeplink";
+
+// Replace with your own ACCESS_KEY and SECRET_KEY
+const ACCESS_KEY = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+const SECRET_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+const REQUEST = { "coupangUrls": [
+        "https://www.coupang.com/np/search?component=&q=good&channel=user", 
+        "https://www.coupang.com/np/coupangglobal"
+]};
+
+(async () => {
+    const authorization = generateHmac(REQUEST_METHOD, URL, SECRET_KEY, ACCESS_KEY);
+    axios.defaults.baseURL = DOMAIN;
+
+    try {       
+        const response = await axios.request({
+            method: REQUEST_METHOD,
+            url: URL,
+            headers: { Authorization: authorization },
+            data: REQUEST
+        });
+        console.log(response.data);
+    } catch (err) {
+        console.error(err.response.data);
+    }    
+})();
