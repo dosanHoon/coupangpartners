eb49bfc7a401e6291dbc519be9c0bb339bf0fe8c
danedehaan <danedehaan@qubitsec.com>
2020-07-17 17:32:08 +0900

    실제키로 테스트 완료

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-const axios = require('axios');
-const { generateHmac } = require('./hmacGenerator');
+const axios = require("axios");
+const { generateHmac } = require("./hmacGenerator");
+const { ACCESS_KEY, SECRET_KEY } = require("./keys");
 
 const REQUEST_METHOD = "POST";
 const DOMAIN = "https://api-gateway.coupang.com";
 const URL = "/v2/providers/affiliate_open_api/apis/openapi/v1/deeplink";
 
-// Replace with your own ACCESS_KEY and SECRET_KEY
-const ACCESS_KEY = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
-const SECRET_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-
-const REQUEST = { "coupangUrls": [
-        "https://www.coupang.com/np/search?component=&q=good&channel=user", 
-        "https://www.coupang.com/np/coupangglobal"
-]};
+const REQUEST = {
+  coupangUrls: [
+    "https://www.coupang.com/np/search?component=&q=good&channel=user",
+    "https://www.coupang.com/np/coupangglobal",
+  ],
+};
 
 (async () => {
-    const authorization = generateHmac(REQUEST_METHOD, URL, SECRET_KEY, ACCESS_KEY);
-    axios.defaults.baseURL = DOMAIN;
+  const authorization = generateHmac(
+    REQUEST_METHOD,
+    URL,
+    SECRET_KEY,
+    ACCESS_KEY
+  );
+  axios.defaults.baseURL = DOMAIN;
 
-    try {       
-        const response = await axios.request({
-            method: REQUEST_METHOD,
-            url: URL,
-            headers: { Authorization: authorization },
-            data: REQUEST
-        });
-        console.log(response.data);
-    } catch (err) {
-        console.error(err.response.data);
-    }    
+  try {
+    const response = await axios.request({
+      method: REQUEST_METHOD,
+      url: URL,
+      headers: { Authorization: authorization },
+      data: REQUEST,
+    });
+    console.log(response.data);
+  } catch (err) {
+    console.error(err.response.data);
+  }
 })();
