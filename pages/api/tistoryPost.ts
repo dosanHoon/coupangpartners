07ea91716e03510a3d41f7210403413b17c60379
danedehaan <danedehaan@qubitsec.com>
2020-07-23 17:30:37 +0900

    티스토리에 포스팅 하는 기능

--- pages/api/tistoryPost.ts
+++ pages/api/tistoryPost.ts
@@ -4,29 +4,48 @@ const tistoryPost = async (req, res) => {
   const browser = await puppeteer.launch({ headless: false });
   try {
     const postdata = req.body.postdata;
+    const title = req.body.title;
     const page = await browser.newPage();
     await page.goto(
       `https://autosellpost.tistory.com/manage/newpost/?type=post&returnURL=%2Fmanage%2Fposts%2F`
     );
     // 네이버 로그인
-    
     await page.click('[type="submit"]');
     await page.waitFor(".textarea_tit");
+    await page.waitFor("#mceu_18-open");
 
-    await page.mouse.click(252, 238);
+    await page.click("#mceu_18-open");
+    await page.click("#mceu_32");
 
-    console.log("postdata", postdata);
-    await page.keyboard.type(postdata, { delay: 100 });
+    await page.waitFor("#editorContainer > div.html-editor");
 
-    await page.type(".textarea_tit", "골드 박스 목록 입니다.", { delay: 100 });
+    await page.keyboard.type(
+      `<style>
+            td {
+              padding: 10px;
+              border: 1px solid grey;
+            }
+            td.rank {
+              min-width: 30px;
+            }
+            td.price {
+              padding: 10px;
+            }
+          </style>` +
+        postdata +
+        "<p>파트너스 활동을 통해 일정액의 수수료를 제공받을 수 있음</p>"
+    );
+    await page.type(".textarea_tit", title, { delay: 10 });
     await page.click(".btn.btn-default");
     await page.waitFor("#open20");
     await page.click("#open20");
     await page.click('[type="submit"]');
+    res.status(200).json({ returnMessage: "성공", returnCode: 0 });
   } catch (e) {
     console.log("tistroy error=============", e);
   } finally {
     await browser.close();
+    res.status(200).json({ returnMessage: "성공", returnCode: 0 });
   }
 };
 
