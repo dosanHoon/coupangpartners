916ca22f691e5999b97cdc3f4593fe0746c5e238
dosanhoon <hangru1106@naver.com>
2020-07-29 16:10:36 +0900

    쿠팡에서 키워드 및 리뷰 크롤링

---
+++ pages/api/getNewKeyword.ts
@@ -0,0 +1,69 @@
+import puppeteer from "puppeteer";
+
+export default async (req, res) => {
+  const browser = await puppeteer.launch({ headless: false });
+  try {
+    const paramCategory = req.query.category;
+    console.log("paramCategory", paramCategory);
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+    });
+    await page.goto(`https://www.itemscout.io/category`);
+
+    const categoryBtn = await page.$(
+      "#container > div.main-container > table:nth-child(1) > tr > td.options > div > div.dropdown-container > div:nth-child(1) > span"
+    );
+
+    await categoryBtn.click();
+    await page.waitFor(1000);
+
+    const categorys = await page.$$eval(
+      "label.itemscout-dropdown-item",
+      (labels) => {
+        console.log("labels", labels);
+        return labels.map((label, i) => {
+          return {
+            label: label.innerText,
+            i,
+          };
+        });
+      }
+    );
+
+    const currentCategory = categorys.find(({ label }) => {
+      return paramCategory === label;
+    });
+    let keywords = [];
+
+    if (currentCategory) {
+      const selectLabel = await page.$$("label.itemscout-dropdown-item");
+
+      await selectLabel[currentCategory.i].click();
+      await page.waitFor(2000);
+      keywords = await page.$$eval(
+        "#keyword-table-scroll-wrapper > table > tbody > tr",
+        (keywords) =>
+          keywords.map((keyword, i) => ({
+            keyword: keyword.querySelector("td:nth-child(3) > a > label")
+              .innerText,
+          }))
+      );
+      console.log("keywords", keywords);
+    } else {
+      keywords = [];
+    }
+
+    await page.waitFor(1000);
+    await browser.close();
+
+    res.json({ returnMessage: "성공", returnCode: 0, data: keywords });
+  } catch (e) {
+    await browser.close();
+    console.log("catch 에러 입니다.", e);
+    res
+      .status(200)
+      .json({ returnMessage: "catch 에러 입니다.", returnCode: -1 });
+  }
+};
---
+++ pages/newkeyword.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Head from "next/head";
+import Axios from "axios";
+
+import {
+  Container,
+  createStyles,
+  makeStyles,
+  Theme,
+  GridList,
+  GridListTile,
+  ListSubheader,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      // overflow: "hidden",
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      // width: 500,
+      // height: 450,
+    },
+    icon: {
+      color: "rgba(255, 255, 255, 0.54)",
+    },
+  })
+);
+
+export default function Home() {
+  const classes = useStyles();
+  const [goldBoxList, setGoldBoxList] = React.useState([]);
+
+  React.useEffect(() => {
+    Axios.get(`/api/getNewKeyword?category=패션의류`).then(({ data }) => {
+      console.log("data", data.data);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <Head>
+        <title>쿠팡 파트너스</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container maxWidth="md">
+        <h1>쇼핑 인사이트 로그</h1>
+        <p>새로운 키워드 추천</p>
+
+        <main>
+          <div className={classes.root}></div>
+        </main>
+      </Container>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
